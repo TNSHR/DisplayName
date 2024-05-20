f9d7fb2df1865e6a487e2b7ebdfb743f4ee86aa3
TNSHR <rohandw0@gmail.com>
2024-05-20 10:14:10 +0530

    final code

--- my-app/src/displayName.jsx
+++ my-app/src/displayName.jsx
@@ -1,73 +1,44 @@
 import React,{useState} from "react";
 
+const DisplayName=()=>{
+    
+    const[firstName, setFirstName]=useState('');
+    const [lastName,setLastName]=useState('');
+    const[fullName,setFullName]=useState('');
+    const HandleTheInput=(event)=>{
+        event.preventDefault();
+        if (firstName && lastName){
+            setFullName(`${firstName} ${lastName}`)
 
-    const DisplayName=()=>{
-        const[firstName, setFirstName]=useState('');
-        const [lastName,setLastName]=useState('');
-        const[fullName,setFullName]=useState('');
-        const[error,setError]=useState({});
-
-        const HandleSubmit=(submit)=>{
-            submit.preventDefault();
-           
-            let errors={};
-            if(!firstName){
-               
-                errors["firstName"]="Please fill this feild"
-            }
-            if(!lastName){
-              
-                errors["lastName"]="Please fill this feild"
-            }
-            setError(errors);
-
-            if (Object.keys(errors).length===0){
-                setFullName(`${firstName} ${lastName}`)
-            }
         }
-        const handleTheFirstNameChange=(e)=>{
-
-            setFirstName(e.target.value);
-            if(e.target.value){
-                setError((prevError)=>({...prevError,firstName:''}))
-            }
-        }
-        const handleTheLastNameChange=(e)=>{
-
-            setFirstName(e.target.value);
-            if(e.target.value){
-                setError((prevError)=>({...prevError,lastName:''}))
-            }
-        }
-
-        return(<>
-        <div style={{class:"container"}}>
-         <form onSubmit={HandleSubmit}>
-            <div style={{padding:"10px"}}> 
-             <label>First Name:</label>
-             <input style={{type:"text"}} onChange={handleTheFirstNameChange}/>
-             {error.firstName && <div>{error.firstName}</div>}
-             
-            </div>
-           <div style={{padding:"10px"}}>
-           <label>Last Name:</label>
-            <input style={{type:"text"}} onChange={handleTheLastNameChange}/>
-            {error.lastName && <div>{error.lastName}</div>}
-           </div>
-          
-            <button style={{type:"button"}} >
-                Submit
-            </button>
-           
-           
-            </form>
-            {fullName && <div>{fullName}</div>}
-
-        </div>
-        </>);
 
     }
+    const handleError=(abc)=>{
+        abc.target.setCustomValidity("Please Fill out this feild");
+    }
+    const handleIn=(abc)=>{
+        abc.target.setCustomValidity("Please Fill out this feild");
+    }
 
+
+    
+
+
+return(
+    <div>
+        <form onClick={HandleTheInput}>
+            <div><span>First Name:</span>
+            <input type="text" value={firstName} onChange={(e)=>setFirstName(e.target.value)} 
+            required onInvalid={handleError} onInput={handleIn} /></div>
+            <div><span>Last Name:</span>
+            <input type="text" value={lastName} onChange={(e)=>setLastName(e.target.value)}
+            required onInvalid={handleError} onInput={handleIn}/></div>
+            
+<button type="submit">Submit</button>
+        </form>
+        {fullName && <div>Full Name: {fullName}</div>}
+    </div>
+)}
 export default DisplayName;
 
 
